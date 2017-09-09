e2008c5c2f83c399d0fbf0f697bbd732ce2cfeb5
juninmd <jr_acn@yahoo.com.br>
2017-09-08 23:24:52 -0300

    iniciando vizinhança

--- br.com.interfatecs/src/Vizinhanca.java
+++ br.com.interfatecs/src/Vizinhanca.java
@@ -1,13 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author jr_ac
- */
+import java.util.Scanner;
+
 public class Vizinhanca {
-    
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int coluna = scanner.nextInt();
+        int largura = scanner.nextInt();
+        String[][] lotes = new String[coluna][largura];
+
+        for (int c = 0; c < coluna; c++) {
+            for (int l = 0; l < largura; l++) {
+                lotes[c][l] = scanner.next();
+            }
+        }
+
+        for (int c = 0; c < coluna; c++) {
+            for (int l = 0; l < largura; l++) {
+                System.out.println(lotes[c][l]);
+            }
+        }
+    }
 }
